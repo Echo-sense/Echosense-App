33e412cbfdd5beb414eea589de139fe9c4871a3d
Kohsuke Hirano <kthirano@uci.edu>
2020-01-29 15:43:26 -0800

    Simulator in py
    make sure to have both insec and sim in the same directory to run.
    Still needs more lasers and updates on compareDistance once more lasers are implemented

---
+++ sim.py
@@ -0,0 +1,126 @@
+import tkinter as tk
+import time
+import math
+from insec import *
+
+#thingy1 dimensions 50x50
+
+WIDTH = 1200
+HEIGHT = 1000
+
+
+class thingy1(object):
+    def __init__(self, canvas, vx, vy, spawnx, spawny):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(spawnx-25, spawny+25, spawnx+25, spawny-25, fill="red")
+        self.vx = vx
+        self.vy = vy
+        self.x = spawnx
+        self.y = spawny
+
+    def move(self):
+        x1, y1, x2, y2 = self.canvas.bbox(self.id)
+        if x2 > WIDTH:
+            self.vx *= -1
+        if x1 < 0:
+            self.vx *= -1
+        if y2 > HEIGHT:
+            self.vy *= -1
+        if y1 < 0:
+            self.vy *= -1
+        self.canvas.move(self.id,self.vx,self.vy)
+
+    def givesides(self):
+        x1, y1, x2, y2 = self.canvas.bbox(self.id)
+        return [((x1, y1),(x2, y1)),((x1,y1),(x1,y2)),((x2,y1),(x2,y2)),((x1,y2),(x2,y2))]
+
+
+class App(object):
+    def __init__(self,master):
+        self.master = master
+        self.canvas = tk.Canvas(root, width = WIDTH, height = HEIGHT)
+        self.canvas.pack()
+
+        self.me = self.canvas.create_rectangle(550,400,650,600, fill="blue")
+
+
+        self.base = (600, 600)
+
+        self.distance = []
+
+        #ADD LIDAR LINES HERE, left to right
+        self.lasers = [(self.base, (0, 600)),
+                      (self.base, (600, 1000)),
+                      (self.base, (1200,600))]
+
+        
+        for dims in self.lasers:
+            self.canvas.create_line(dims[0][0], dims[0][1], dims[1][0], dims[1][1])
+            self.distance.append(10000)
+        
+        self.things = [thingy1(self.canvas, 1,1,300,600)]
+        
+        self.canvas.pack()
+        self.master.after(0,self.animation)
+
+    #determines distance from object if there is one in field of view
+    def scan(self):
+        disArray = []
+        for laser in self.lasers:
+            mindist = 10000
+            for obj in self.things: #for each object
+                sides = obj.givesides()
+                for side in sides: #for each side of the object
+                    ic = intersects(side, laser)
+                    if (ic == (-1,-1)):
+                        tempd = 10000
+                    else:
+                        tempd = math.sqrt((self.base[0] - ic[0])**2 + (self.base[1]-ic[1])**2)
+                    #print(tempd)
+                    if tempd < mindist:
+                        mindist = tempd
+            disArray.append(mindist)
+        return disArray
+
+    def compareDistance(self, curframe):
+        for x in range(len(curframe)):
+            #CHANGE THIS AFTER MORE LASERS
+            if (curframe[x] < self.distance[x]):
+                self.canvas.itemconfig(self.me, fill='red')
+                
+
+    #moves the GUI by one frame and makes calculations, calls itself when it's done
+    def animation(self):
+        self.canvas.itemconfig(self.me, fill = 'blue')
+        for thing in self.things:
+            thing.move()
+        currentframe = self.scan()
+        self.compareDistance(currentframe)
+        self.distance = currentframe
+        self.master.after(10,self.animation)
+
+
+    
+
+
+#def intersects(line1, line2):
+#    xdif = ( line1[0][0]-line1[1][0] , line2[0][0]-line2[1][0] )
+#    ydif = ( line1[0][1]-line1[1][1] , line2[0][1]-line2[1][1] )
+
+#    def det(dif1,dif2):
+#        return dif1[0] * dif2[1] - dif1[1]*dif2[0]
+#    
+#    res = det(xdif, ydif)
+#    if res == 0:
+#        return 10000
+#    d = (det(*line1), det(*line2))
+#    x = det(d,xdif) / res
+#    y = det(d,ydif) / res
+#    return (x,y)
+    
+
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    canvas = App(root)
+    root.mainloop()
